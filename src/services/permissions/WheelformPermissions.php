642c2b81ee15a21e0e93331b0c35da80fa5e12f9
Roger Orantes <roger@wheelinteractive.com>
2022-05-06 12:30:34 -0500

    close #261

--- src/services/permissions/WheelformPermissions.php
+++ src/services/permissions/WheelformPermissions.php
@@ -12,23 +12,26 @@ class WheelformPermissions extends Component
     {
         $forms = Form::find()->where(['active' => 1])->all();
         $permissions = [
-            'wheelform_new_form' => [
-                'label' => Craft::t('wheelform', 'Create new forms'),
-            ],
-            'wheelform_manage_all_forms' => [
-                'label' => Craft::t('wheelform', 'Manage all forms'),
-                'nested' => [
-                    'wheelform_view_all_forms_entries' => [
-                        'label' => Craft::t('wheelform', 'View all forms entries'),
-                    ],
-                    'wheelform_change_all_forms_settings' => [
-                        'label' => Craft::t('wheelform', 'Change all forms settings'),
-                    ],
-                ]
-            ],
+            'heading' => "Wheelform",
+            'permissions' => [
+                'wheelform_new_form' => [
+                    'label' => Craft::t('wheelform', 'Create new forms'),
+                ],
+                'wheelform_manage_all_forms' => [
+                    'label' => Craft::t('wheelform', 'Manage all forms'),
+                    'nested' => [
+                        'wheelform_view_all_forms_entries' => [
+                            'label' => Craft::t('wheelform', 'View all forms entries'),
+                        ],
+                        'wheelform_change_all_forms_settings' => [
+                            'label' => Craft::t('wheelform', 'Change all forms settings'),
+                        ],
+                    ]
+                ],
+            ]
         ];
         foreach($forms as $form) {
-            $permissions['wheelform_edit_form_'.$form->id] = [
+            $permissions['permissions']['wheelform_edit_form_'.$form->id] = [
                 'label' => Craft::t('wheelform', 'Manage') . ' - ' . $form->name,
                 'nested' => [
                     'wheelform_view_entries_' . $form->id => [
