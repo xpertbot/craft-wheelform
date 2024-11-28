49405a6b333d71520bd782df25ac0cb3e9417172
Roger Orantes <roger@wheelinteractive.com>
2024-11-28 14:57:10 -0600

    v3.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 3.2.2 - 2024-11-28
+
+### Fix
+- Craft request parameter not working correctly
 
 ## 3.2.1 - 2024-04-18
 
--- src/controllers/MessageController.php
+++ src/controllers/MessageController.php
@@ -259,7 +259,7 @@ class MessageController extends BaseController
             'errors' => [],
             'data' => [],
             'headers' => [],
-            'redirect' => $this->request->getValidatedBodyParam('redirect'),
+            'redirect' => Craft::$app->request->getValidatedBodyParam('redirect'),
             'entry' => null,
         ];
 
@@ -353,7 +353,7 @@ class MessageController extends BaseController
             if ($default !== null) {
                 $url = $default;
             } else {
-                $url = $this->request->getPathInfo();
+                $url = Craft::$app->request->getPathInfo();
             }
         } else if ($object) {
             $url = $this->getView()->renderObjectTemplate($url, $object);
